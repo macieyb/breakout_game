502425ed2c73dc61bc170a112f0f307f6bcb4d2e
maciej.babiarz <maciej.babiarz@salesmanago.pl>
2017-12-29 09:08:26 +0100

    wall collision

--- script.js
+++ script.js
@@ -7,15 +7,21 @@ var x = canvas.width / 2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
 
 // --- Draw the ball ---
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#9edd1a";
     ctx.fill();
     ctx.closePath();
+}
+
+// fillStyle color randomizer
+function colorRandomize() {
+    return Math.floor(Math.random() * 16777215).toString(16);
 }
 
 function draw() {
@@ -23,6 +29,14 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     // draw the ball
     drawBall();
+    // bouncing off the top and bottom
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    // bouncing off the left and right
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
     // move after each frame
     x += dx;
     y += dy;
