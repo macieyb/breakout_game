0ac440314767f5696c668b7dccda37b6bcaae235
maciej.babiarz <maciej.babiarz@salesmanago.pl>
2018-01-05 17:46:48 +0100

    bounce, bricks and simple collision detection

--- script.js
+++ script.js
@@ -10,15 +10,69 @@ var dy = -2;
 var ballRadius = 10;
 var ballColor = "#9edd1a";
 var paddleColor = "#0095DD";
-var paddleHeight = 20;
-var paddleWidth = 95;
+var paddleHeight = 10;
+var paddleWidth = 75;
 var paddleX = (canvas.width - paddleWidth) / 2;
 var rightPressed = false;
 var leftPressed = false;
 
+// --- Brick variables ---
+
+var brickRowCount = 4;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+var brickColor = "#0095DD";
+
+var bricks = [];
+for (c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = {x: 0, y: 0, status: 1};
+    }
+}
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
+function keyDownHandler(e) {
+    if (e.keyCode === 39) {
+        rightPressed = true;
+    }
+    else if (e.keyCode === 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.keyCode === 39) {
+        rightPressed = false;
+    }
+    else if (e.keyCode === 37) {
+        leftPressed = false;
+    }
+}
+
+// --- Collision detection ---
+
+function collisionDetection() {
+    for (c = 0; c < brickColumnCount; c++) {
+        for (r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
+            // calculations
+            if (b.status === 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    ballColor = "#" + colorRandomize();
+                }
+            }
+        }
+    }
+}
 
 // --- Draw the ball ---
 
@@ -40,7 +94,28 @@ function drawPaddle() {
     ctx.closePath();
 }
 
-// fillStyle color randomizer
+// --- Draw bricks ---
+
+function drawBricks() {
+    for (c = 0; c < brickColumnCount; c++) {
+        for (r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status === 1) {
+                var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = brickColor;
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+// --- fillStyle color randomizer ---
+
 function colorRandomize() {
     return Math.floor(Math.random() * 16777215).toString(16);
 }
@@ -48,14 +123,27 @@ function colorRandomize() {
 function draw() {
     // clear canvas before each frame
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // draw the ball
+    // draw the ball, bricks and paddle
+    drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection();
     // bouncing off the top and bottom
-    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    if (y + dy < ballRadius) {
         dy = -dy;
         ballColor = "#" + colorRandomize();
     }
+    else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+            ballColor = "#" + colorRandomize();
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+        }
+    }
+
     // bouncing off the left and right
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -70,24 +158,6 @@ function draw() {
     }
     else if (leftPressed && paddleX > 0) {
         paddleX -= 7;
-    }
-}
-
-function keyDownHandler(e) {
-    if (e.keyCode == 39) {
-        rightPressed = true;
-    }
-    else if (e.keyCode == 37) {
-        leftPressed = true;
-    }
-}
-
-function keyUpHandler(e) {
-    if (e.keyCode == 39) {
-        rightPressed = false;
-    }
-    else if (e.keyCode == 37) {
-        leftPressed = false;
     }
 }
 
